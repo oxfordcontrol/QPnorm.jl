1c213d893bfc951e727b0b5cd3d6931e8a742bd8
nrontsis <nrontsis@gmail.com>
2019-03-05 18:45:42 +0000

    Scripts for obtaining nytimes bag of words and saving it as a sparse int matrix to a jld2 file.

---
+++ examples/docword_to_sparse_matrix.jl
@@ -0,0 +1,10 @@
+using CSV
+using DataFrames
+using JLD2
+
+df = CSV.File("docword.nytimes.txt", header=0, datarow=4, delim=" ") |> DataFrame
+I = convert(Vector{Int32}, df[:Column1])
+J = convert(Vector{Int32}, df[:Column2])
+V = convert(Vector{Int16}, df[:Column3])
+D = sparse(I, J, V)
+@save "docword_nytimes.jld2" D
---
+++ examples/download_nytimes_docword.sh
@@ -0,0 +1,2 @@
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz
+gunzip docword.nytimes.txt.gz
