1db5dc672bdd14834141692bcfd86febb81758a5
nrontsis <nrontsis@gmail.com>
2019-03-07 12:33:03 +0000

    Add link for downloading the vocabulary.

--- examples/download_nytimes_docword.sh
+++ examples/download_nytimes_docword.sh
@@ -1,2 +1,3 @@
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/docword.nytimes.txt.gz
+wget http://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nytimes.txt
 gunzip docword.nytimes.txt.gz
